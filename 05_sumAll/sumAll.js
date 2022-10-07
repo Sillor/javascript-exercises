b836fbc2a1a7a8526c46d2d0f8a7b272c4906853
Egor <egorstrah95@gmail.com>
2022-10-07 15:52:56 -0700

    Done 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let sum = 0;
+    if (x >= 0 && y >= 0 && 
+        typeof x === 'number' && typeof y === 'number') {
+        if (x < y) {
+            for (let i = x; i <= y; i++)
+                sum += i;
+        }
+        else {
+            for (let i = x; i >= y; i--)
+                sum += i;
+        }
+        return sum;
+    }
+    else return 'ERROR';
 };
 
 // Do not edit below this line
