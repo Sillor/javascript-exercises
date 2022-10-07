cfa9fb8124fa419aec1d209f36414286aebc1175
Egor <egorstrah95@gmail.com>
2022-10-07 15:32:16 -0700

    Done 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...x) {
+    for (let i = 0; i < x.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === x[i]) {
+                arr.splice(j, 1);
+                break;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
