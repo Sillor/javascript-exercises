2bfcb45be1481f0843f690ad35ba3706cbd7de06
Egor <egorstrah95@gmail.com>
2022-10-07 15:11:32 -0700

    Done 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let returnString = "";
+    if (n >= 0) {
+        for (let i = 0; i < n; i++) {
+            returnString += string;
+        }
+    }
+    else returnString = "ERROR";
+    return returnString;
 };
 
 // Do not edit below this line
