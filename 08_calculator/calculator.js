bf84b4e53a4b4bce8910f8e7492f5f7b2e7d338d
Egor <egorstrah95@gmail.com>
2022-10-07 16:13:33 -0700

    Done 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let total = x;
+  for (let i = 1; i < y; i++) {
+    total *= x;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  return x <= 1 ? 1 : x * factorial(x - 1);
 };
 
 // Do not edit below this line
