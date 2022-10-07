5637a0652728d94b339dea3252be722ada8d7e9d
Egor <egorstrah95@gmail.com>
2022-10-07 16:27:24 -0700

    Done 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let backwards = "";
+    let clean = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        if (string.charAt(i).toUpperCase() !=
+            string.charAt(i).toLowerCase()) {
+                backwards += string.charAt(i);
+            }
+    }
+    for (let i = 0; i <= string.length - 1; i++) {
+        if (string.charAt(i).toUpperCase() !=
+            string.charAt(i).toLowerCase()) {
+                clean += string.charAt(i);
+            }
+    }
+    return (backwards.toLowerCase() === clean.toLowerCase());
 };
 
 // Do not edit below this line
